65e65dd83a262bee1515d5e01af0a20a78de6083
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-12 14:03:23 -0800

    Write and FAIL test for coutning dipthings

--- __tests__/haikuChecker.test.js
+++ __tests__/haikuChecker.test.js
@@ -1,7 +1,13 @@
-import { countVowels, countSilentVowels, countDipAndTripThongs } from '../src/js/haikuChecker.js'
+import { countVowels, countSilentVowels, countDipthongs } from '../src/js/haikuChecker.js'
 
 describe ('countVowels', () => {
   test('should return the correct number of vowels in a sentence', () => {
     expect(countVowels("I am Vanessa")).toEqual(5);
+  });
+});
+
+describe ('countDipthongs', () => {
+  test('should return the correct number of dipthongs in a sentence', () => {
+    expect(countDipthongs("beautiful keeper")).toEqual(2);
   });
 });
--- src/js/haikuChecker.js
+++ src/js/haikuChecker.js
@@ -1,15 +1,42 @@
 // Count the vowels in the sentence
 export const countVowels = (string) => {
-  const vowelsArray = string.match(/[aeiou]/gi);
-  return vowelsArray.length;
+  return string.match(/[aeiouy]/gi).length;
 };
+
+
+// Subtract one vowel when two vowel sounds form one speech sound
+export const countDipthongs = (string) => {
+  // return string.match(/[aeiouy][aeiouy]/gi).length
+};
+
+
 
 // Subtract silent vowels
 export const countSilentVowels = (string) => {
-
+  const dipthongArray = string.match(/[aeiouy][aeiouy]/gi);
 };
 
-// Subtract one vowel when two vowel sounds form one speech sound
-export const countDipAndTripThongs = (string) => {
+// Rules for Silent A
+// Rule: A is not pronounced in many adverbs, where the words ends in –ally. For example: stoically, logically.
 
-};
+// Rules for Silent E
+// Rule 1: If the letter E comes at the ends of words, it is generally not pronounced.
+// • Fore, table, before, write, give, hide.
+
+// Rule 2: If E occurs before the letter D in the second and third forms of the verbs, E may sometimes not be pronounced.
+// • Bored, fixed, smuggled, begged.
+
+// Rules for Silent U
+// Rule: The letter U is not pronounced when it comes after G and before a vowel in a word.
+// • Guide, guest, guard, guess, guano.
+
+// Exception for Silent I
+// business
+
+// Rules for when Y is considered a vowel
+// Y is considered to be a vowel if…
+// The word has no other vowel: gym, my.
+// The letter is at the end of a word or syllable: candy, deny, bicycle, acrylic.
+// The letter is in the middle of a syllable: system, borborygmus.
+// Subtract silent vowels
+
