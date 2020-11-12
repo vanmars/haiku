f60edd7c4d5def405ab08a1bdc4cf77c39219523
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-12 14:16:43 -0800

    PASS test for counting silent vowels

--- src/js/haikuChecker.js
+++ src/js/haikuChecker.js
@@ -10,10 +10,10 @@ export const countDipthongs = (string) => {
 
 // Count silent vowels
 export const countSilentVowels = (string) => {
-  // const silentA = string.match(/ally\b/gi).length;
-  // const silentE = string.match(/e\b/gi).length;
-  // const anotherSilentE = string.match(/ed\b/gi).length;
-  // const silentU = string.match(/gu[aeiouy]/gi).length;
-  // const oddSiltentI = string.match(/business/gi).length;
-  // return silentA + silentE + anotherSilentE + silentU + oddSiltentI
+  const silentA = string.match(/ally\b/gi).length;
+  const silentE = string.match(/e\b/gi).length;
+  const anotherSilentE = string.match(/ed\b/gi).length;
+  const silentU = string.match(/gu[aeiouy]/gi).length;
+  const oddSiltentI = string.match(/business/gi).length;
+  return silentA + silentE + anotherSilentE + silentU + oddSiltentI
 };
