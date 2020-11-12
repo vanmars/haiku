446b1e5566a46d8102e0dcca2db3b10975f34986
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-12 14:15:51 -0800

    Write and FAIL test for counting silent vowels

--- __tests__/haikuChecker.test.js
+++ __tests__/haikuChecker.test.js
@@ -1,13 +1,19 @@
 import { countVowels, countSilentVowels, countDipthongs } from '../src/js/haikuChecker.js'
 
 describe ('countVowels', () => {
-  test('should return the correct number of vowels in a sentence', () => {
+  test('should return the correct number of vowels in a string', () => {
     expect(countVowels("I am Vanessa")).toEqual(5);
   });
 });
 
 describe ('countDipthongs', () => {
-  test('should return the correct number of dipthongs in a sentence', () => {
+  test('should return the correct number of dipthongs in a string', () => {
     expect(countDipthongs("beautiful keeper")).toEqual(2);
+  });
+});
+
+describe ('countSilentVowels', () => {
+  test('should return the correct number of silent vowels in a string', () => {
+    expect(countSilentVowels("guide drastically business fixed")).toEqual(5);
   });
 });
--- src/js/haikuChecker.js
+++ src/js/haikuChecker.js
@@ -3,40 +3,17 @@ export const countVowels = (string) => {
   return string.match(/[aeiouy]/gi).length;
 };
 
-
-// Subtract one vowel when two vowel sounds form one speech sound
+// Countwhen two vowel sounds form one speech sound
 export const countDipthongs = (string) => {
   return string.match(/[aeiouy][aeiouy]/gi).length
 };
 
-
-
-// Subtract silent vowels
+// Count silent vowels
 export const countSilentVowels = (string) => {
-  const dipthongArray = string.match(/[aeiouy][aeiouy]/gi);
+  // const silentA = string.match(/ally\b/gi).length;
+  // const silentE = string.match(/e\b/gi).length;
+  // const anotherSilentE = string.match(/ed\b/gi).length;
+  // const silentU = string.match(/gu[aeiouy]/gi).length;
+  // const oddSiltentI = string.match(/business/gi).length;
+  // return silentA + silentE + anotherSilentE + silentU + oddSiltentI
 };
-
-// Rules for Silent A
-// Rule: A is not pronounced in many adverbs, where the words ends in –ally. For example: stoically, logically.
-
-// Rules for Silent E
-// Rule 1: If the letter E comes at the ends of words, it is generally not pronounced.
-// • Fore, table, before, write, give, hide.
-
-// Rule 2: If E occurs before the letter D in the second and third forms of the verbs, E may sometimes not be pronounced.
-// • Bored, fixed, smuggled, begged.
-
-// Rules for Silent U
-// Rule: The letter U is not pronounced when it comes after G and before a vowel in a word.
-// • Guide, guest, guard, guess, guano.
-
-// Exception for Silent I
-// business
-
-// Rules for when Y is considered a vowel
-// Y is considered to be a vowel if…
-// The word has no other vowel: gym, my.
-// The letter is at the end of a word or syllable: candy, deny, bicycle, acrylic.
-// The letter is in the middle of a syllable: system, borborygmus.
-// Subtract silent vowels
-
