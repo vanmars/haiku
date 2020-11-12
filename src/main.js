3a5792d736c0a32a3e91fa6367346613e92f6503
Vanessa Stewart <vstewart1213@gmail.com>
2020-11-12 15:32:18 -0800

    Add details about why poem failed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import * as haiku from './js/haikuChecker.js';
+import {checkHaiku, countSyllables} from './js/haikuChecker.js';
 
 $(document).ready(function(){
   $('#haiku-form').submit(function(event){
@@ -10,15 +10,16 @@ $(document).ready(function(){
     const inputtedLine1 = $('#line1').val();
     const inputtedLine2 = $('#line2').val();
     const inputtedLine3 = $('#line3').val();
-
-    const result = haiku.checkHaiku(inputtedLine1, inputtedLine2, inputtedLine3);
-
+    const result = checkHaiku(inputtedLine1, inputtedLine2, inputtedLine3);
     if (result) {
       $('#results-div').show();
       $('#results-div').html(`<h4>You have a Haiku!</h4><br><p>${inputtedLine1}</p><p>${inputtedLine2}</p><p>${inputtedLine3}</p>`);
     } else {
+      const Line1Count = countSyllables(inputtedLine1);
+      const Line2Count = countSyllables(inputtedLine2);
+      const Line3Count = countSyllables(inputtedLine3);
       $('#results-div').show();
-      $('#results-div').html(`<h4>Wah wah! You do NOT have a Haiku!</h4>`);
+      $('#results-div').html(`<h4>Wah wah! You do NOT have a Haiku!</h4><p>Line 1 is ${Line1Count} syllables, and should be 5.</p><p>Line 2 is ${Line2Count} syllables, and should be 7.</p><p>Line 3 is ${Line3Count} syllables, and should be 5.</p>`);
     }
   });
 });
